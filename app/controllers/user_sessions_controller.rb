1fd3d934640f6d262934ebbc6d5c386db82e5a38
Kevin Luo <kevin.luo@telus.com>
2023-06-04 14:54:30 -0400

    add logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,4 +14,9 @@ class UserSessionsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/secret'
-  resources :user_sessions, only: [:new, :create]
+  resources :user_sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :new, :create]
 end
